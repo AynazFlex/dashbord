637cc76be473ff1c9e2e04a618ac040bfef3540b
Aynaz <jkz1713x2@gmail.com>
2025-12-31 21:40:02 +0300

    finish

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices } from "@/app/lib/data";
+import { revalidatePath } from "next/cache";
 
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
@@ -53,10 +54,18 @@ export default async function LatestInvoices() {
             );
           })}
         </div>
-        <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
-        </div>
+        <form
+          action={async () => {
+            "use server";
+
+            revalidatePath("/dashboard");
+          }}
+        >
+          <button className="flex items-center pb-2 pt-6">
+            <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+            <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          </button>
+        </form>
       </div>
     </div>
   );
